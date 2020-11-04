a52363729b8cf280bebd7e91f7fe8f94f8ebacbe
Ameer Sameer <ammeer_atteya@yahoo.com>
2020-11-04 22:51:51 +0200

    BuyNProductsGetXPercentageOnProductDiscount

--- src/Discount/BuyNProductsGetXPercentageOnProductDiscount.php
+++ src/Discount/BuyNProductsGetXPercentageOnProductDiscount.php
@@ -2,29 +2,53 @@
 
 namespace App\Discount;
 
+use App\Fixture\BuyNProductsGetXPercentageOnProductDiscountFixture;
 use App\Fixture\ProductFixture;
 
 class BuyNProductsGetXPercentageOnProductDiscount extends AbstractDiscount
 {
+    private $N;
+    private $NProducts;
+    private $rate;
+    private $product;
+
+    public function getDiscounts(): array
+    {
+        return (new BuyNProductsGetXPercentageOnProductDiscountFixture)->loadData();
+    }
+
+    public function setParams($params): void
+    {
+        $this->N = $params['N'];
+        $this->NProducts = $params['products'];
+        $this->rate = $params['rate'];
+        $this->product = $params['product'];
+    }
+
     /**
      *  checks if the the discount is applicable to a list of products, if yes
      *  it returns the discount value, else it return 0
      */
-    public function check(): float
+    public function getDiscountValue(): float
     {
-        $jacketPrice = (new ProductFixture())->loadData()['Jacket'];
-        $tShirtsCount = 0;
-        $jacketDiscountsCount = 0;
-        $jacketsCount = array_count_values($this->products)['Jacket'] ?? 0;
+        $productPrice = (new ProductFixture())->loadData()[$this->product];
+        $NProductsCount = 0;
+        $productDiscountsCount = 0;
+        $productCount = array_count_values($this->products)[$this->product] ?? 0;
         foreach ($this->products as $product) {
-            if ($product == "T-shirt") {
-                $tShirtsCount++;
-                if ($tShirtsCount != 0 && $tShirtsCount % 2 == 0 && $jacketDiscountsCount < $jacketsCount) {
-                    $jacketDiscountsCount++;
+            if ($product == $this->NProducts) {
+                $NProductsCount++;
+                if ($NProductsCount != 0 && $NProductsCount % $this->N == 0 && $productDiscountsCount < $productCount) {
+                    $productDiscountsCount++;
                 }
             }
         }
 
-        return ($jacketDiscountsCount * $jacketPrice * 0.5);
+        return ($productDiscountsCount * $productPrice * $this->rate);
+    }
+
+    public function getDiscountName(): string
+    {
+        return ($this->rate * 100) . '% off ' . $this->product;
     }
 }
